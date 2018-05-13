e88a6cc2ccec5199948aa616fd2657c04a3ef160
liuyingjie <handanyingjie@sina.com>
2018-05-13 17:09:01 +0800

    merge to demo1

--- app/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2018/5/13 0013
- * Time: 12:57
- */
-echo 1;
--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2018/5/13 0013
- * Time: 12:51
- */
-require_once './vendor/autoload.php';
-require './app/test.php';
